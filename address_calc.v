dfc46bf716ded31ba16f1b55761b316b569ad06a
alvarogd16 <alvarogarcia16102000@gmail.com>
2022-03-12 13:51:40 +0100

    Added ram memory in addres_calc

--- address_calc.v
+++ address_calc.v
@@ -1,40 +1,50 @@
 `include "modules/adder.v"
 `include "modules/mux2.v"
 `include "modules/register.v"
+`include "modules/ram.v"
 
 `timescale 1s/1ms
 
-// Maquina Rudimentaria
-module cpu;
+// --------- Maquina Rudimentaria - Calculo de direcciones ---------
+module address_calc;
 
-reg [15:0] ir_in;   // Entrada al registro de instrucción
+// ---- Sañales de entrada auxiliares ----
 
 reg [15:0] regb_out;
 
-wire [7:0] mux_1_out;
-
-
-// Señales de control
+// ---- Señales de control ----
 reg ld_ir,
     ld_rdir,
     ld_pc,
     mux_1_pc,
     reset_pc_sel,
+    mem_w;
 
 reg clk = 0;
 
-// Señales intermedias
-wire [15:0] ir_out
+// ---- Señales intermedias ----
+wire [15:0] ir_out,
+            mem_out;
 
 wire [7:0] add_dir_out,
             rdir_out,
             inc_out,
             pc_out,
-            pc_in;
+            pc_in,
+            mem_dir;
 
+// --- Modulos ----
+
+ram #(.AW(8), .DW(16), .ROM_FILE("modules/mem_files/rom_test.mem")) MEM (
+    .clk(clk),
+    .w(mem_w),
+    .addr(mem_dir),
+    .data_in(regb_out),
+    .data_out(mem_out)
+);
 
 register #(.N(16)) IR (
-    .in(ir_in),
+    .in(mem_out),
     .ld(ld_ir),
     .clk(clk),
     .out(ir_out)
@@ -55,7 +65,7 @@ register RDIR (
 
 adder INC (
     .in_a(8'd1),
-    .in_b(mux_1_out),
+    .in_b(mem_dir),
     .out(inc_out)
 );
 
@@ -77,43 +87,43 @@ mux2 MUX_1 (
     .in_a(pc_out),
     .in_b(rdir_out),
     .sel(mux_1_pc),
-    .out(mux_1_out)
+    .out(mem_dir)
 );
 
 
 always # 0.5 clk = ~clk;
 
 initial begin
-    $dumpfile("cpu.vcd");
-    $dumpvars(0, cpu);
+    $dumpfile("address_calc.vcd");
+    $dumpvars(0, address_calc);
 
-    // Initialize variables
-    ir_in = 'hFFAA;
+    // // Initialize variables
+    // ir_in = 'hFFAA;
 
-    // Señales de control
-    ld_ir = 1;      // Cargo la instrucción
-    ld_rdir = 0;
-    ld_pc = 1;
-    mux_1_pc = 0;
-    reset_pc_sel = 1;
+    // // Señales de control
+    // ld_ir = 1;      // Cargo la instrucción
+    // ld_rdir = 0;
+    // ld_pc = 1;
+    // mux_1_pc = 0;
+    // reset_pc_sel = 1;
 
 
-    # 1
-    ld_ir = 0;
-    ld_rdir = 1;
-    reset_pc_sel = 0;
+    // # 1
+    // ld_ir = 0;
+    // ld_rdir = 1;
+    // reset_pc_sel = 0;
 
-    # 2
-    mux_1_pc = 1;
+    // # 2
+    // mux_1_pc = 1;
 
-    # 1
-    mux_1_pc = 0;
+    // # 1
+    // mux_1_pc = 0;
 
-    # 5
-    reset_pc_sel = 1;
+    // # 5
+    // reset_pc_sel = 1;
 
-    # 1
-    reset_pc_sel = 0;
+    // # 1
+    // reset_pc_sel = 0;
     
     # 10;
     $finish;
